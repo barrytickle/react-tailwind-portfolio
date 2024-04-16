50439bae99c58d474c55891af490ed6b58cd51dd
unknown <barry.tickle12@gmail.com>
2024-04-16 23:24:59 +0100

    Progress

--- src/components/largeText.jsx
+++ src/components/largeText.jsx
@@ -13,11 +13,11 @@ export default function LargeText({ details }) {
 
   return (
     <>
-      <Wrapper className={"text-4xl text-white max-w-4xl mx-auto font-light mb-8"}>
+      <Wrapper className={"text-4xl text-white max-w-4xl mx-auto font-light mb-8 px-4"}>
         <p className=" text-left z-30 inline-flex items-center justify-center px-6 py-2 text-sm font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-800 to-dark-900">
           {details.tag}
         </p>
-        <h2 className={"text-6xl font-medium mb-8"}>{details.title}</h2>
+        <h2 className={"text-6xl font-extrabold mb-8"}>{details.title}</h2>
         <p className={positionMap[details.position] ?? ""}>{details.content}</p>
       </Wrapper>
     </>
