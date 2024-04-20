b494d66efefbffac19a1eb44418997079398e20b
unknown <barry.tickle12@gmail.com>
2024-04-20 18:17:32 +0100

    Modified the featured case studies component to show all components if required

--- src/components/featuredCaseStudies.jsx
+++ src/components/featuredCaseStudies.jsx
@@ -1,30 +1,60 @@
 import ButtonSolidRound from "./buttons/buttonSolidRound";
-import { url } from "../helpers/config";
+import { endpoint, url } from "../helpers/config";
 import ButtonAnimated from "./buttons/buttonAnimated";
+import { useEffect, useState } from "react";
 
 function FeaturedCaseStudies({ details }) {
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      if (pages.length > 0) return;
+
+      if (!details.showAllCards) {
+        setPages(details?.pages?.data);
+        return;
+      }
+
+      const caseStudies = await fetch(`${endpoint}/pages/?filters[category][slug][$eq]=case-studies&populate=deep`);
+
+      const res = await caseStudies.json();
+      if (res.data && res.data.length > 0) setPages(res.data);
+    })();
+  }, [pages]);
+
+  console.log("Pages", pages);
+
   return (
     <section className="px-4 pt-12 pb-10 md:pb-12 md:pt-16  sm:px-6 lg:px-8">
       <div className="w-full max-w-screen-xl mx-auto">
         {/* Featured work section header */}
         <div className="w-full max-w-xl mx-auto text-center lg:max-w-3xl md:max-w-2xl">
-          <p className="inline-flex items-center justify-center px-6 py-2 text-sm font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-600 to-dark-700">
-            {details.tag}
-          </p>
+          {details.tag ? (
+            <p className="inline-flex items-center justify-center px-6 py-2 text-sm font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-600 to-dark-700">
+              {details.tag}
+            </p>
+          ) : (
+            ""
+          )}
 
           <h2 className="mt-6 text-3xl font-extrabold text-white sm:text-4xl md:text-5xl">{details.title}</h2>
-          <ButtonSolidRound
-            className={"w-[fit-content] mx-auto mt-8"}
-            url="case-studies"
-            text="View all case studies"
-          />
+
+          {details.buttonText ? (
+            <ButtonSolidRound
+              className={"w-[fit-content] mx-auto mt-8"}
+              url="case-studies"
+              text="View all case studies"
+            />
+          ) : (
+            ""
+          )}
         </div>
 
         {/* Featured work */}
         <div className="mt-12 space-y-12 lg:mt-16 lg:space-y-56 lg:pb-24">
           {/* Work item */}
 
-          {details.pages.data.map((caseStudy, ind) => {
+          {pages.map((caseStudy, ind) => {
             let header = caseStudy.attributes.blocks.filter((f) => f.__component === "components.case-study-header");
             if (header.length === 0) return <p key={ind}>No case study found</p>;
 
@@ -58,7 +88,7 @@ function FeaturedCaseStudies({ details }) {
                       </div>
                       <div className="mt-8 xl:mt-4 flex">
                         <ButtonAnimated
-                          url={`${caseStudy.attributes?.category?.data?.attributes?.slug}/${caseStudy?.attributes?.slug}`}
+                          url={`/${caseStudy.attributes?.category?.data?.attributes?.slug}/${caseStudy?.attributes?.slug}`}
                           text={"Case study"}
                         />
 
