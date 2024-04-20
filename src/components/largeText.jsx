993eef19ca665678f26b77c1ba6072e389c6c448
Barry Tickle <barry.tickle12@gmail.com>
2024-04-20 03:05:47 +0100

    Merge Conflict

--- src/components/largeText.jsx
+++ src/components/largeText.jsx
@@ -13,6 +13,7 @@ export default function LargeText({ details }) {
 
   return (
     <>
+<<<<<<< HEAD
       <Wrapper className={"text-4xl text-white max-w-4xl mx-auto font-light mb-8"}>
         <section className="px-4 py-12 md:py-16 sm:px-6 lg:px-8">
           <div className="max-w-screen-md mx-auto text-center lg:max-w-screen-xl">
@@ -32,6 +33,14 @@ export default function LargeText({ details }) {
             </div>
           </div>
         </section>
+=======
+      <Wrapper className={"text-4xl text-white max-w-4xl mx-auto font-light mb-8 px-4"}>
+        <p className=" text-left z-30 inline-flex items-center justify-center px-6 py-2 text-sm font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-800 to-dark-900">
+          {details.tag}
+        </p>
+        <h2 className={"text-6xl font-extrabold mb-8"}>{details.title}</h2>
+        <p className={positionMap[details.position] ?? ""}>{details.content}</p>
+>>>>>>> 50439ba (Progress)
       </Wrapper>
     </>
   );
