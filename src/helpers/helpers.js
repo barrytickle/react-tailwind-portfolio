49a8d96907d9cca866151d0ad2e5dcd7f93941ad
Barry Tickle <barry.tickle12@gmail.com>
2024-04-20 11:28:15 +0100

    Fixed issue with nav

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -2,7 +2,7 @@ export const getCategoryFromPage = (page) => page.attributes.category.data.attri
 
 export const linkClicked = (e) => {
   if (window.innerWidth < 768) {
-    document.querySelector("header nav")?.querySelector("input").click();
+    document.querySelector("header nav")?.querySelector("input:checked").click();
     document.querySelector("header nav")?.querySelector("label")?.classList.remove("active");
     document.querySelector("body")?.classList.remove("no-move");
   }
