456d62ccd08e4d25b835b855050bee128b828d3e
unknown <barry.tickle12@gmail.com>
2024-04-21 23:58:06 +0100

    Fixed layout of case studies

--- src/components/featuredCaseStudies.jsx
+++ src/components/featuredCaseStudies.jsx
@@ -62,48 +62,88 @@ function FeaturedCaseStudies({ details }) {
 
             header = header[0];
 
-            console.log("HEADER", header);
+            if (ind % 2 == 0)
+              return (
+                <div key={ind}>
+                  <div className="relative max-w-lg mx-auto lg:max-w-none lg:mx-none">
+                    <div className="relative w-full h-64 bg-center shadow-xl lg:rounded-3xl rounded-t-3xl lg:pt-[46%] lg:w-[90%] lg:h-full">
+                      <img
+                        src={url + header?.featuredImage?.data?.attributes?.formats?.large?.url}
+                        alt={caseStudy.attributes?.name}
+                        className="absolute inset-0 object-cover object-center w-full h-full lg:rounded-3xl rounded-t-3xl"
+                      />
+                      <p className="inline-flex items-center justify-center px-6 py-2 mt-4 ml-6 text-xs font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r sm:ml-8 lg:hidden from-dark-800 to-dark-900">
+                        Website
+                      </p>
+                    </div>
+                    <div className="relative bottom-0 right-0 z-20 block w-full h-auto p-6 shadow-xl lg:rounded-3xl rounded-b-3xl sm:p-8 lg:p-12 bg-dark-700 lg:absolute lg:-bottom-25 lg:w-1/3 lg:h-full">
+                      <div className="h-full lg:flex lg:flex-col lg:justify-between">
+                        <div className="hidden lg:block">
+                          <p className="inline-flex items-center justify-center px-6 py-2 text-xs font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-800 to-dark-900">
+                            Website
+                          </p>
+                        </div>
+                        <div className="flex flex-wrap w-full">
+                          <h3 className="text-3xl font-bold text-white lg:text-4xl">{caseStudy.attributes?.name}</h3>
+                          <p className="mt-2 text-base lg:mt-4 text-dark-300 md:text-lg">{header?.description}</p>
+                        </div>
+                        <div className="mt-8 xl:mt-4 flex">
+                          <ButtonAnimated
+                            url={`/${caseStudy.attributes?.category?.data?.attributes?.slug}/${caseStudy?.attributes?.slug}`}
+                            text={"Case study"}
+                          />
 
-            return (
-              <div key={ind}>
-                <div className="relative max-w-lg mx-auto lg:max-w-none lg:mx-none">
-                  <div className="relative w-full h-64 bg-center shadow-xl lg:rounded-3xl rounded-t-3xl lg:pt-[46%] lg:w-[90%] lg:h-full">
-                    <img
-                      src={url + header?.featuredImage?.data?.attributes?.formats?.large?.url}
-                      alt="Case study 1"
-                      className="absolute inset-0 object-cover object-center w-full h-full lg:rounded-3xl rounded-t-3xl"
-                    />
-                    <p className="inline-flex items-center justify-center px-6 py-2 mt-4 ml-6 text-xs font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r sm:ml-8 lg:hidden from-dark-800 to-dark-900">
-                      Website
-                    </p>
-                  </div>
-                  <div className="relative bottom-0 right-0 z-20 block w-full h-auto p-6 shadow-xl lg:rounded-3xl rounded-b-3xl sm:p-8 lg:p-12 bg-dark-700 lg:absolute lg:-bottom-25 lg:w-1/3 lg:h-full">
-                    <div className="h-full lg:flex lg:flex-col lg:justify-between">
-                      <div className="hidden lg:block">
-                        <p className="inline-flex items-center justify-center px-6 py-2 text-xs font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-800 to-dark-900">
-                          Website
-                        </p>
-                      </div>
-                      <div className="flex flex-wrap w-full">
-                        <h3 className="text-3xl font-bold text-white lg:text-4xl">{caseStudy.attributes?.name}</h3>
-                        <p className="mt-2 text-base lg:mt-4 text-dark-300 md:text-lg">{header?.description}</p>
-                      </div>
-                      <div className="mt-8 xl:mt-4 flex">
-                        <ButtonAnimated
-                          url={`/${caseStudy.attributes?.category?.data?.attributes?.slug}/${caseStudy?.attributes?.slug}`}
-                          text={"Case study"}
-                        />
-
-                        <ButtonSolidRound
-                          url={header?.url}
-                          text="Visit Website"
-                        />
+                          <ButtonSolidRound
+                            url={header?.url}
+                            text="Visit Website"
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
+              );
+
+            if (ind % 2 !== 0)
+              return (
+                <div key={ind}>
+                  <div className="relative max-w-lg mx-auto lg:max-w-none lg:mx-none">
+                    <div className="relative w-full h-64 bg-center bg-cover shadow-xl lg:rounded-3xl rounded-t-3xl lg:pt-[46%] lg:w-[90%] lg:ml-auto lg:h-full">
+                      <img
+                        src={url + header?.featuredImage?.data?.attributes?.formats?.large?.url}
+                        className="absolute inset-0 object-cover object-center w-full h-full lg:rounded-3xl rounded-t-3xl"
+                      />
+                      <p className="inline-flex items-center justify-center px-6 py-2 mt-4 ml-6 text-xs font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r sm:ml-8 lg:hidden from-dark-800 to-dark-900">
+                        Website
+                      </p>
+                    </div>
+                    <div className="relative bottom-0 left-0 z-20 block w-full h-auto p-6 shadow-xl lg:rounded-3xl rounded-b-3xl sm:p-8 lg:p-12 bg-dark-700 lg:h-full lg:absolute lg:-bottom-25 lg:w-1/3">
+                      <div className="h-full lg:flex lg:flex-col lg:justify-between">
+                        <div className="hidden lg:block">
+                          <p className="inline-flex items-center justify-center px-6 py-2 text-xs font-medium tracking-wide text-white rounded-r-full rounded-tl-full bg-gradient-to-r from-dark-800 to-dark-900">
+                            Website
+                          </p>
+                        </div>
+                        <div className="flex flex-wrap w-full">
+                          <h3 className="text-3xl font-bold text-white lg:text-4xl">{caseStudy.attributes?.name}</h3>
+                          <p className="mt-2 text-base lg:mt-4 text-dark-300 md:text-lg">{header?.description}</p>
+                        </div>
+                        <div className="mt-8 xl:mt-4 flex">
+                          <ButtonAnimated
+                            url={`/${caseStudy.attributes?.category?.data?.attributes?.slug}/${caseStudy?.attributes?.slug}`}
+                            text={"Case study"}
+                          />
+
+                          <ButtonSolidRound
+                            url={header?.url}
+                            text="Visit Website"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
           })}
         </div>
       </div>
