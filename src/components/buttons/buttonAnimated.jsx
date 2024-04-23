cdc13d9b10812e9528f15e9c0cb93eb1c8e49090
unknown <barry.tickle12@gmail.com>
2024-04-23 12:12:11 +0100

    Removed global classes from button

--- src/components/buttons/buttonAnimated.jsx
+++ src/components/buttons/buttonAnimated.jsx
@@ -5,7 +5,7 @@ function ButtonAnimated(props) {
   return (
     <Link
       onClick={linkClicked}
-      className={`relative flex items-center justify-center w-auto px-8 ml-0 text-base font-medium leading-snug text-white h-14 sm:ml-4 sm:mt-0 group ${props.className ?? ""}`}
+      className={`relative flex items-center justify-center w-auto px-8 ml-0 text-base font-medium leading-snug text-white h-14 sm:mt-0 group ${props.className ?? ""}`}
       to={props.url}>
       <span className="absolute top-0 left-0 z-10 block w-full h-full transition-all ease-in-out rounded-full bg-gradient-to-r duration-250 from-dark-800 to-dark-900 md:w-14 md:group-hover:w-full"></span>
       <span className="relative z-20">{props.text}</span>
