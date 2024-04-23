2e8e09ca0c9f5efae80d8f11be51ee6accc25593
unknown <barry.tickle12@gmail.com>
2024-04-23 12:08:58 +0100

    Ammends

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ function Hero({ details }) {
                 if (button.style === "whiteRound")
                   return (
                     <ButtonSolidRound
-                      className={"mt-2"}
+                      className={"mt-2 md:mt-0"}
                       key={ind}
                       url={button.url}
                       text={button.label}
